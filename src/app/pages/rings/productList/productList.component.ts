b1092664337ab7692697ca9e7430c6727fde1ff7
RabiaBeker <137835970+RabiaBeker@users.noreply.github.com>
2025-01-23 22:52:12 +0300

    changed product detail code

--- src/app/pages/rings/productList/productList.component.ts
+++ src/app/pages/rings/productList/productList.component.ts
@@ -1,19 +1,14 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, Input,  OnInit,  ViewEncapsulation } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RingService} from "../ring.service";
 import { Product } from '../../../model/products';
 import {MatTableModule} from '@angular/material/table';
-import {CdkTableDataSourceInput} from "@angular/cdk/table";
 import {MatCard} from "@angular/material/card";
 import {MatIcon} from "@angular/material/icon";
 import {MatFormField} from "@angular/material/form-field";
 import {FormsModule} from "@angular/forms";
 import {MatIconButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
-
-
-
-
 
 @Component({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -23,15 +18,14 @@ import {MatInput} from "@angular/material/input";
   providers: [],
   templateUrl: './productList.component.html',
   styleUrl: './productList.component.css',
-
 })
 
 export class ProductListComponent {
 
-  @Input() selectedProduct: Product;
-
-  public readonly data: Product;
+  public data: Product;
   public products: Product [] = [];
+  public count: number = 1;
+  public price: number = 0;
 
   constructor(private readonly ringService: RingService)
   {
@@ -41,17 +35,20 @@ export class ProductListComponent {
     }
   }
 
-  count: number = 0;
-  price: number;
-
-  increase(item: Product) {
+  public increase(item: Product): void {
     this.count++;
-    this.price = item.price * this.count;
+    this.calculatePriceOfProduct(item, 'increase')
   }
 
-  decrease(item: Product) {
-    if (this.count > 0) {
-      this.count--;
+  public decrease(item: Product): void {
+    this.count--;
+    this.calculatePriceOfProduct(item, 'decrease')
+  }
+
+  private calculatePriceOfProduct(item: Product, action: string): void {
+    if (action === 'increase') {
+      this.price = item.price * this.count;
+    } else {
       this.price = this.price - item.price;
     }
   }
