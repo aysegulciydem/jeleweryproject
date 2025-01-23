58024f63ac7018e3007f0b23dc4e0451be8734c2
RabiaBeker <137835970+RabiaBeker@users.noreply.github.com>
2025-01-23 20:59:38 +0300

    changed product detail code

--- src/app/pages/rings/productList/productList.component.ts
+++ src/app/pages/rings/productList/productList.component.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 import {RingService} from "../ring.service";
 import { Product } from '../../../model/products';
 import {MatTableModule} from '@angular/material/table';
+import {CdkTableDataSourceInput} from "@angular/cdk/table";
+import {MatCard} from "@angular/material/card";
+import {MatIcon} from "@angular/material/icon";
+import {MatFormField} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatIconButton} from "@angular/material/button";
+import {MatInput} from "@angular/material/input";
 
 
 
@@ -12,25 +19,42 @@ import {MatTableModule} from '@angular/material/table';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-productList',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatCard, MatIcon, MatFormField, FormsModule, MatIconButton, MatInput],
   providers: [],
   templateUrl: './productList.component.html',
   styleUrl: './productList.component.css',
-  
+
 })
 
 export class ProductListComponent {
-  products: Product [] = [];
-  public readonly data: Product;
+
   @Input() selectedProduct: Product;
 
-  constructor(private readonly ringService: RingService){
-    this.data = this.ringService.getData();
-    this.products.push(this.data)
-    console.log(this.products)
-  }
-  
+  public readonly data: Product;
+  public products: Product [] = [];
 
-  displayedColumns: string[] = ['id', 'name', 'price', 'imageUrl'];
-  dataSource = this.products;
+  constructor(private readonly ringService: RingService)
+  {
+    this.data = this.ringService.getData();
+    if (this.data) {
+      this.products.push(this.data)
+    }
+  }
+
+  count: number = 0;
+  price: number;
+
+  increase(item: Product) {
+    this.count++;
+    this.price = item.price * this.count;
+  }
+
+  decrease(item: Product) {
+    if (this.count > 0) {
+      this.count--;
+      this.price = this.price - item.price;
+    }
+  }
+
+
 }
