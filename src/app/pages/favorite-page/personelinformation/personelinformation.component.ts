ad2832b275125927b3eea30e03bc796c8b4e1de7
aysegulciydem <aysegulciydem@gmail.com>
2025-08-01 20:43:37 +0300

    update

--- src/app/pages/favorite-page/personelinformation/personelinformation.component.ts
+++ src/app/pages/favorite-page/personelinformation/personelinformation.component.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import {DatePipe} from '@angular/common';
 
 @Component({
     selector: 'app-personelinformation',
-    imports: [MatTabsModule, CommonModule, BrowserAnimationsModule, MatFormFieldModule, MatInputModule,],
+    imports: [CommonModule, MatTabsModule, DatePipe],
     templateUrl: './personelinformation.component.html',
     styleUrl: './personelinformation.component.css'
 })
 export class PersonelinformationComponent {
+
+    tabLoadTimes: Date[] = [];
+
+  getTimeLoaded(index: number) {
+    if (!this.tabLoadTimes[index]) {
+      this.tabLoadTimes[index] = new Date();
+    }
+
+    return this.tabLoadTimes[index];
+  }
 
 }
